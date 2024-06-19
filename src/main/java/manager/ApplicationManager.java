374df7467372222b92a1f83084d2413c5ecda4ba
Lidiia90 <liduska200412@gmail.com>
2024-06-19 19:54:41 +0300

    created project structure

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -1,4 +1,25 @@
 package manager;
 
-public class AplicationManager {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class ApplicationManager {
+    private WebDriver driver;
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.navigate().to("https://demoqa.com/");
+    }
+    @AfterMethod
+    public void tearDown(){
+        if(driver != null)
+        driver.quit();
+    }
 }
